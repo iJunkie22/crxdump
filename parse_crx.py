7a6088df15541ea2c26b98d63f02d5cfa2743831
Ethan Randall <iJunkie22@gmail.com>
2016-05-26 19:14:44 -0400

    added comments

--- parse_crx.py
+++ parse_crx.py
@@ -8,7 +8,7 @@ import os.path
 
 
 def main():
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2:  # Print usage and abort
         print('Usage', sys.argv[0], '<filename.crx>')
         sys.exit(1)
 
@@ -29,6 +29,7 @@ def main():
 
         print('PKZip starts at', f.tell())
         zf = zipfile.ZipFile(f)
+
         print()
         print(os.path.basename(crx_dir))
         print('\t' + '\n\t'.join(zf.namelist()))
@@ -38,7 +39,7 @@ def main():
             if p1 != '' and not os.path.isdir(os.path.join(crx_dir, p1)):
                 os.mkdir(os.path.join(crx_dir, p1))
 
-            if p2 == '':
+            if p2 == '':    # This was only a directory name
                 continue
 
             outfile = open(os.path.join(crx_dir, p1, p2), 'wb')

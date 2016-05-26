6a3aa92317716ca3f1b2ed6474c501bd84e139e6
Ethan Randall <iJunkie22@gmail.com>
2016-05-26 18:31:16 -0400

    added fail-safe file descriptor closing (PEP  310)

--- parse_crx.py
+++ parse_crx.py
@@ -11,24 +11,30 @@ def main():
         print('Usage', sys.argv[0], '<filename.crx>')
         sys.exit(1)
     f = open(sys.argv[1], 'rb')
-    assert (f.read(4) == 'Cr24')
-    version = struct.unpack('I', f.read(4))[0]
-    key_size = struct.unpack('I', f.read(4))[0]
-    sig_size = struct.unpack('I', f.read(4))[0]
-    key = f.read(key_size)
-    sig = f.read(sig_size)
-    print('PKZip starts at', f.tell())
-    zf = zipfile.ZipFile(f)
-    print(zf.namelist())
-    os.mkdir(os.path.basename(sys.argv[1]))
-    os.chdir(os.path.basename(sys.argv[1]))
-    for filename in zf.namelist():
-        if filename.endswith('/'):
-            os.mkdir(filename)
-            continue
-        outfile = open(filename, 'wb')
-        outfile.write(zf.read(filename))
-        outfile.close()
+    try:
+        assert (f.read(4) == 'Cr24')
+        version = struct.unpack('I', f.read(4))[0]
+        key_size = struct.unpack('I', f.read(4))[0]
+        sig_size = struct.unpack('I', f.read(4))[0]
+        key = f.read(key_size)
+        sig = f.read(sig_size)
+        print('PKZip starts at', f.tell())
+        zf = zipfile.ZipFile(f)
+        print(zf.namelist())
+        os.mkdir(os.path.basename(sys.argv[1]))
+        os.chdir(os.path.basename(sys.argv[1]))
+        for filename in zf.namelist():
+            if filename.endswith('/'):
+                os.mkdir(filename)
+                continue
+            outfile = open(filename, 'wb')
+            try:
+                outfile.write(zf.read(filename))
+            finally:
+                outfile.close()
+
+    finally:
+        f.close()
 
 if __name__ == "__main__":
     main()
